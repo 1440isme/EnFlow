be041caee4ad7a96ffb94179a589aa479835cdea
1440isme <23110184@student.hcmute.edu.vn>
2026-03-16 22:27:36 +0700

    Initial commit

---
+++ src/main/java/vn/enflow/EnFlowApplication.java
@@ -0,0 +1,13 @@
+package vn.enflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnFlowApplication.class, args);
+    }
+
+}
